dcdd18d2cd322aa3591cc1f26f288b93fb037e02
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-10-17 13:01:43 +0530

    fetching env var from file

--- lib/kinde.ts
+++ lib/kinde.ts
@@ -1,6 +1,6 @@
 export const config = {
-	clientId: "5650e7b8005e4ed986c35a8f595e6c0f",
-	domain: "https://aishelfbook.kinde.com",
-	redirectUri: "http://localhost:5173/create-account",
-	logoutUri: "http://localhost:5173/login",
+	clientId: process.env.KINDE_CLIENT_ID!,
+	domain: process.env.KINDE_ISSUER_URL!,
+	redirectUri: process.env.KINDE_POST_LOGIN_REDIRECT_URL!,
+	logoutUri: process.env.KINDE_POST_LOGOUT_REDIRECT_URL!,
 };
